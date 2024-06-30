38f4d682d1fbf75190ac29dfcc6358ad9c97a84b
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-30 10:10:03 -0700

    stubbed route for /api/notes

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const path = require("path");
 // serve static files (CSS, JS, HTML) from the public directory
 
 // add static middleware to serve assets from the public folder
-//app.use(express.static("public"));
 app.use(express.static("public"));
 
-
+app.get("/api/notes", (req, res) => {
+  console.log(`call to api " + ${req.url}`);
+  res.json("handling api call");
+});
 
 app.listen(PORT, () => {
   console.log(`serving static routes at http://localhost:${PORT}`);
